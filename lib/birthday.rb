b07c11f4d87cc84c544c1cc128c2a7bdc363261c
Bobby Neal <rsneal01@gmail.com>
2020-05-07 17:01:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -18,5 +18,6 @@ def age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       puts "You are too hold for this."
+    end
   end
 end
